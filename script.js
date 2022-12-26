ef7faf2587da8ca079a34d0c58756fd6f5566adf
matisagardia <matisagardia7@gmail.com>
2022-12-26 15:55:44 -0300

    scrolling at learn more button

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+    e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// smooth scrolling, with the getboundingclientrect method we get the coordinates of the button that we are clicking
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', (e) => {
+    //getting coordinates
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords);
+    console.log(window.scrollX, scrollY);
+    //scrolling
+    section1.scrollIntoView({behavior:'smooth'});
+});
