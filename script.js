70049998cbf4b44b9cf9ea4cfa0f4601cc8823e6
matisagardia <matisagardia7@gmail.com>
2022-12-28 09:48:36 -0300

    sticky bar getBoundingClientRect

--- script.js
+++ script.js
@@ -127,12 +127,20 @@ nav.addEventListener('mouseout', e => handleHover(e, 1));
 
 // Sticky navigation
 
-const initialCoords = section1.getBoundingClientRect();
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
 
-window.addEventListener('scroll', function (e) {
-  if(window.scrollY > initialCoords.top) {
-    nav.classList.add('sticky');
-  } else {
-    nav.classList.remove('sticky');
-  }
+
+const stickyNav = function (entries){
+  const [entry] = entries;
+  if(!entry.isIntersecting)  nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+
+headerObserver.observe(header);
